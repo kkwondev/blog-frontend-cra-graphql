77955f4e3f8ce5c46d8d0e1d6e73180479f8a52d
kkwondev <kkwoncokr@gmail.com>
2021-05-04 14:32:18 +0900

    Separate the response request

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,11 +1,21 @@
 import { User } from './User';
 
-export interface Post {
+export interface requestPost {
     title: string;
     content: string;
     // eslint-disable-next-line camelcase
     thumnbnail_img?: string;
     categoryName: string;
     tags: string[];
+    user: User;
+}
+
+export interface responsePost {
+    title: string;
+    content: string;
+    // eslint-disable-next-line camelcase
+    thumnbnail_img?: string;
+    category: string;
+    createdAt: string;
     user: User;
 }
