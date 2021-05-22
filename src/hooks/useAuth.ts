07be0eaa571b3849098af920ee76bae8cc165f4f
kkwon <kkwoncokr@gmail.com>
2021-05-22 17:30:18 +0900

    Add action on login failure

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,7 +1,7 @@
-import { useMutation } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import { useSetRecoilState } from 'recoil';
 import { userState } from '../atoms/authState';
-import { GOOGLE_LOGIN } from '../lib/apollo/queries/auth';
+import { CURRENT_USER, GOOGLE_LOGIN } from '../lib/apollo/queries/auth';
 import { User } from '../types/User';
 
 interface loginData {
@@ -20,12 +20,18 @@ export default function useAuth() {
 
     const login = ({ accessToken }: any) => {
         // TODO: 백엔드와 연결 성공 2021-05-22
-        googleLogin({ variables: { access_token: accessToken } }).then(response => {
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            const { user, access_token } = response.data!.google;
-            setUser(user);
-            localStorage.setItem('Authorization', `Bearer ${access_token}`);
-        });
+        googleLogin({ variables: { access_token: accessToken } })
+            .then(response => {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                const { user, access_token } = response.data!.google;
+                setUser(user);
+                localStorage.setItem('Authorization', `Bearer ${access_token}`);
+            })
+            .catch(e => {
+                // eslint-disable-next-line no-alert
+                console.error(e);
+                alert('로그인이 실패하였습니다.');
+            });
     };
 
     const logout = () => {
