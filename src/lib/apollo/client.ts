635ed33f0b4f7ab46be8eea77678d93a9e193022
kkwon <kkwoncokr@gmail.com>
2021-05-22 17:21:37 +0900

    If you have authorization in local storage, add it to the header on request

--- src/lib/apollo/client.ts
+++ src/lib/apollo/client.ts
@@ -1,15 +1,27 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, concat, createHttpLink, InMemoryCache } from '@apollo/client';
 
-const host = (process.env.NODE_ENV === 'development' ? '/' : process.env.REACT_APP_API_HOST) || '/';
+const host =
+    (process.env.NODE_ENV === 'development' ? 'http://localhost:3001/' : process.env.REACT_APP_API_HOST) || '/';
 
 const graphqlURI = host.concat('graphql');
 const link = createHttpLink({
     uri: graphqlURI,
-    credentials: 'include',
+    // credentials: true,
+});
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+    // add the authorization to the headers
+    operation.setContext({
+        headers: {
+            Authorization: localStorage.getItem('Authorization') || null,
+        },
+    });
+
+    return forward(operation);
 });
 
 const client = new ApolloClient({
-    link,
+    link: concat(authMiddleware, link),
     cache: new InMemoryCache(),
 });
 
